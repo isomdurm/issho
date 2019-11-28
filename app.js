bf42b7005a534d14045503737936c3d0985dfe69
isomdurm <isom@isom.tech>
2019-11-28 13:34:15 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello darkness my old friend"));
+
+app.use(passport.initialize());
+require('./frontend/passport')(passport);
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+app.listen(process.env.PORT || 5000);
